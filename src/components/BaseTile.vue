c6ef9a02ed6ad4dc94ad2072551be53a70b93a84
Shevja <vlsdki@gmail.com>
2024-08-01 21:13:46 +0700

    added scss, added tiles, added board (without logic)

---
+++ src/components/BaseTile.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { defineExpose, defineProps } from 'vue';
+defineExpose({ test })
+
+const emit = defineEmits(['onClick'])
+
+const props = defineProps({
+    id: {
+        type: Number,
+        require: true,
+    }
+})
+
+function test() {
+    console.log(`tile - ${props.id} clicked`)
+}
+</script>
+
+<template>
+    <div class="tile" @click="emit('onClick', props.id)"></div>
+</template>
+
+<style lang="scss" scoped>
+.tile {
+    filter: hue-rotate(10rad);
+
+    width: 100%;
+    height: 100%;
+    background-color: red;
+    border-radius: 6px;
+    box-shadow: 0px 0px 5px $gray inset;
+    opacity: .7;
+
+    transition: box-shadow .05s;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 1;
+    }
+
+    &:active {
+        box-shadow: 0px 0px 15px $gray inset;
+        opacity: .95;
+    }
+
+    &_active {
+        box-shadow: 0px 0px 15px $gray inset;
+        opacity: .95;
+    }
+}
+</style>
---
+++ src/components/TheBoard.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref, onMounted, reactive } from 'vue';
+import BaseTile from './BaseTile.vue';
+
+const props = defineProps({
+    tilesCount: {
+        default: 4,
+        type: Number,
+    },
+    dificult: {
+        default: 0,
+        type: Number,
+    },
+})
+
+const tiles = ref({})
+
+const sequence = ref([])
+
+onMounted(() => {
+    sequence.value.push(getRandomNumber(props.tilesCount))
+    // showSequence()
+    console.log(tiles[0])
+})
+
+function tileClicked(id) {
+    console.log(id)
+    console.log(getRandomNumber(props.tilesCount), 'rand')
+    console.log(tiles.value[id - 1].test())
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max) + 1
+}
+
+function showSequence() {
+    sequence.value.forEach(tileId => {
+
+    })
+}
+
+</script>
+
+<template>
+    <div class="board">
+        <ul class="board__tiles">
+            <li v-for="tileId in props.tilesCount" :key="tileId" class="board-tile">
+                <BaseTile ref="tiles" :id="tileId" @onClick="tileClicked" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+.board {
+    display: flex;
+    justify-content: center;
+
+    &__tiles {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 5px;
+        width: 100%;
+        max-width: 400px;
+    }
+
+    &-tile {
+        flex: 1 0 calc(50% - 5px);
+        // height: 40px;
+        aspect-ratio: 1 / 1;
+    }
+}
+</style>
